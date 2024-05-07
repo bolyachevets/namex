c3fc9c25214c2de0fb2e7d32718feb12bf82538b
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-05-06 21:21:21 -0700

    remove more vars

--- services/emailer/config.py
+++ services/emailer/config.py
@@ -88,14 +88,11 @@ class Config:  # pylint: disable=too-few-public-methods
     AUTH_API_VERSION = os.getenv("AUTH_API_VERSION", "")
     NOTIFY_API_URL = os.getenv("NOTIFY_API_URL", "")
     NOTIFY_API_VERSION = os.getenv("NOTIFY_API_VERSION", "")
-    BUSINESS_API_URL = os.getenv("BUSINESS_API_URL", "")
-    BUSINESS_API_VERSION = os.getenv("BUSINESS_API_VERSION", "")
     NAMEX_API_URL = os.getenv("NAMEX_API_URL", "")
     NAMEX_API_VERSION = os.getenv("NAMEX_API_VERSION", "")
     PAY_API_URL = os.getenv("PAY_API_URL", "")
     PAY_API_VERSION = os.getenv("PAY_API_VERSION", "")
 
-    LEGAL_API_URL = f"{BUSINESS_API_URL + BUSINESS_API_VERSION}"
     NOTIFY_API_URL = f"{NOTIFY_API_URL + NOTIFY_API_VERSION}/notify"
     NAMEX_SVC_URL = f"{NAMEX_API_URL + NAMEX_API_VERSION}"
     PAY_API_URL = f"{PAY_API_URL + PAY_API_VERSION}/payment-request"
@@ -134,7 +131,6 @@ class Testing(Config):  # pylint: disable=too-few-public-methods
     DEBUG = True
     TESTING = True
     DEPLOYMENT_ENV = "testing"
-    LEGAL_API_URL = "https://legal-api-url/"
     PAY_API_URL = "https://pay-api-url/"
 
 
